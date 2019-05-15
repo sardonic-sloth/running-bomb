ab55f8f15d9615500aee867197e5885ec5e4f8ec
Jason Weir <jweir@cruxconnect.com>
2019-05-14 23:55:00 -0600

    space=jump; enter=enter

--- Exit.gd
+++ Exit.gd
@@ -3,7 +3,7 @@ extends Area2D
 export(String, FILE, '*.tscn') var next_world
 
 func _physics_process(delta):
-	if Input.is_action_pressed('ui_up'):
+	if Input.is_action_pressed('ui_accept') or Input.is_action_pressed('ui_up'):
 		var bodies = get_overlapping_bodies()
 
 		for body in bodies:
