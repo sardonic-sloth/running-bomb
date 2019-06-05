d5049ef6e82dee9ec38ed19b32f9165a59af6e4e
Jason Weir <jweir@cruxconnect.com>
2019-06-04 23:57:45 -0600

    Convert Toph to RigidBody2D

--- Toph.gd
+++ Toph.gd
@@ -1,51 +1,146 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-const UP = Vector2(0, -1)
-const GRAVITY = 20
-const ACCELERATION = 25
-const MAX_SPEED = 400
-const JUMP_HEIGHT = -750
-
-var motion = Vector2()
+var anim = ""
+var facing_left = false
 var jumping = false
+var stopping_jump = false
 
-func _physics_process(delta):
-    var friction = false
+var WALK_ACCEL = 800.0
+var WALK_DEACCEL = 800.0
+var WALK_MAX_VELOCITY = 200.0
+var AIR_ACCEL = 300.0
+var AIR_DEACCEL = 800.0
+var JUMP_VELOCITY = 600
+var STOP_JUMP_FORCE = 900.0
 
-    motion.y += GRAVITY
+var MAX_FLOOR_AIRBORNE_TIME = 0.15
 
-    if Input.is_action_pressed('ui_right'):
-        motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
-        $Sprite.flip_h = false
-        $Sprite.play('Walk')
-    elif Input.is_action_pressed('ui_left'):
-        motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
-        $Sprite.flip_h = true
-        $Sprite.play('Walk')
+var airborne_time = 1e20
+
+var floor_h_velocity = 0.0
+
+func _integrate_forces(state):
+    var lv = state.get_linear_velocity()
+    var step = state.get_step()
+
+    var new_anim = anim
+    var new_facing_left = facing_left
+
+    # Get the controls
+    var move_left = Input.is_action_pressed("move_left")
+    var move_right = Input.is_action_pressed("move_right")
+    var is_jump_pressed = Input.is_action_pressed("jump")
+
+    # Turn on friction
+    if move_left or move_right:
+        physics_material_override.friction = 0
     else:
-        friction = true
-        if abs(motion.x) < (float(MAX_SPEED) / 2):
-            $Sprite.play('Idle')
+        physics_material_override.friction = 1
 
-    if is_on_ceiling():
-        if motion.y <= GRAVITY:
-            motion.y += GRAVITY
+    # Deapply prev floor velocity
+    lv.x -= floor_h_velocity
+    floor_h_velocity = 0.0
 
-    if is_on_floor():
-        if Input.is_action_just_pressed('ui_up') or Input.is_action_just_pressed('ui_select'):
-            jumping = true
-            motion.y = JUMP_HEIGHT
-            $Boing.play()
-        else:
+    # Find the floor (a contact with upwards facing collision normal)
+    var found_floor = false
+    var floor_index = -1
+
+    for x in range(state.get_contact_count()):
+        var ci = state.get_contact_local_normal(x)
+        if ci.dot(Vector2(0, -1)) > 0.6:
+            found_floor = true
+            floor_index = x
+
+    if found_floor:
+        airborne_time = 0.0
+    else:
+        airborne_time += step # Time it spent in the air
+
+    var on_floor = airborne_time < MAX_FLOOR_AIRBORNE_TIME
+
+    # Process jump
+    if jumping:
+        if lv.y > 0:
+            # Set off the jumping flag if going down
             jumping = false
-            motion.y = 0
-        if friction:
-            motion.x = lerp(motion.x, 0, 0.1)
-    else:
-        if motion.y < 0:
-            $Sprite.play('Jump')
-        elif jumping or motion.y > 100:
-            $Sprite.play('Fall')
-        motion.x = lerp(motion.x, 0, 0.02)
+        elif not is_jump_pressed:
+            stopping_jump = true
 
-    motion = move_and_slide(motion, UP)
+        if stopping_jump:
+            lv.y += STOP_JUMP_FORCE * step
+
+    if on_floor:
+        # Process logic when character is on floor
+        if move_left and not move_right:
+            if lv.x > -WALK_MAX_VELOCITY:
+                lv.x -= WALK_ACCEL * step
+        elif move_right and not move_left:
+            if lv.x < WALK_MAX_VELOCITY:
+                lv.x += WALK_ACCEL * step
+        else:
+            var xv = abs(lv.x)
+            xv -= WALK_DEACCEL * step
+            if xv < 0:
+                xv = 0
+            lv.x = sign(lv.x) * xv
+
+        # Check jump
+        if not jumping and is_jump_pressed:
+            lv.y = -JUMP_VELOCITY
+            jumping = true
+            stopping_jump = false
+            $Boing.play()
+
+        # Check facing
+        if lv.x < 0 and move_left:
+            new_facing_left = true
+        elif lv.x > 0 and move_right:
+            new_facing_left = false
+        if jumping:
+            new_anim = "Jump"
+        elif abs(lv.x) < 0.1:
+            new_anim = "Idle"
+        else:
+            new_anim = "Walk"
+    else:
+        # Process logic when the character is in the air
+        if move_left and not move_right:
+            if lv.x > -WALK_MAX_VELOCITY:
+                lv.x -= AIR_ACCEL * step
+        elif move_right and not move_left:
+            if lv.x < WALK_MAX_VELOCITY:
+                lv.x += AIR_ACCEL * step
+        else:
+            var xv = abs(lv.x)
+            xv -= AIR_DEACCEL * step
+            if xv < 0:
+                xv = 0
+            lv.x = sign(lv.x) * xv
+
+        if lv.y < 0:
+            new_anim = "Jump"
+        else:
+            new_anim = "Fall"
+
+    # Update facing
+    if new_facing_left != facing_left:
+        if new_facing_left:
+            $Sprite.flip_h = true
+        else:
+            $Sprite.flip_h = false
+
+        facing_left = new_facing_left
+
+    # Change animation
+    if new_anim != anim:
+        anim = new_anim
+        $Sprite.play(anim)
+
+    # Apply floor velocity
+    if found_floor:
+        floor_h_velocity = state.get_contact_collider_velocity_at_position(floor_index).x
+        lv.x += floor_h_velocity
+
+    # Finally, apply gravity and set back the linear velocity
+    lv += state.get_total_gravity() * step
+    state.set_linear_velocity(lv)
