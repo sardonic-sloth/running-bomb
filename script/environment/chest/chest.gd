f1fc7f6fa18450816e4b34e7b1ff467ee85c8e58
Ben Reece <benreece@gmail.com>
2019-06-11 22:31:56 -0600

    Add chest

---
+++ script/environment/chest/chest.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+export(String, FILE, '*.png') var contents
+
+func _physics_process(delta):
+	if Input.is_action_pressed('ui_accept') and $StaticBody2D.get_overlapping_bodies():
+        open_chest()
+
+func open_chest():
+    $Sprite.play('open')
+    $OpenSound.play()
+
+    var texture = ImageTexture.new()
+    texture.load(contents)
+    $Contents.texture = texture
+    $Contents.get_node('MoveUp').play('Up')
